35cf8e2d433160ca0bcb062a5740592e4778a340
Bruno Henrique Paiva <bruno@sigane.com.br>
2019-04-18 14:04:36 -0300

    Little cleanup SRTF

--- javascript/algorithms/srtf.js
+++ javascript/algorithms/srtf.js
@@ -1,18 +1,35 @@
 const createArrivalTimeComparator = require("../comparators/arrivalTime");
 const createBurstTimeComparator = require("../comparators/burstTime");
 
+const getTotalExecutionTime = processes =>
+  processes.reduce((total, processOne) => total + processOne.burstTime, 0);
+
+const getTotalWaitingTimeProcess = process =>
+  process.executions.reduce(
+    (totalProcess, execution, index) =>
+      totalProcess +
+      execution.start -
+      (index === 0 ? process.arrivalTime : process.executions[index - 1].end),
+    0
+  );
+
+const getTotalWaitingTimeProcesses = processes =>
+  processes.reduce(
+    (total, process) => total + getTotalWaitingTimeProcess(process),
+    0
+  );
+
 const getProcessToExecute = (
   processes,
   currentExecutionTime,
   burstTimeComparator
-) => {
-  return processes
+) =>
+  processes
     .filter(
       process =>
         process.arrivalTime <= currentExecutionTime && process.burstTime !== 0
     )
     .sort(burstTimeComparator)[0];
-};
 
 const srtf = (processes, tiebreakerComparator) => {
   processes = processes
@@ -20,10 +37,7 @@ const srtf = (processes, tiebreakerComparator) => {
     .sort(createArrivalTimeComparator(tiebreakerComparator));
 
   let burstTimeComparator = createBurstTimeComparator(tiebreakerComparator);
-  let totalExecutionTime = processes.reduce(
-    (total, processOne) => total + processOne.burstTime,
-    0
-  );
+  let totalExecutionTime = getTotalExecutionTime(processes);
 
   for (
     let currentExecutionTime = 0;
@@ -37,41 +51,21 @@ const srtf = (processes, tiebreakerComparator) => {
     );
 
     process.executions = process.executions || [];
-    if (process.executions.length > 0) {
-      let lastExecution = process.executions[process.executions.length - 1];
-      if (lastExecution.end === currentExecutionTime) {
-        lastExecution.end++;
-      } else {
-        process.executions.push({
-          start: currentExecutionTime,
-          end: currentExecutionTime + 1
-        });
-      }
+
+    let lastExecution = process.executions[process.executions.length - 1];
+    if (lastExecution && lastExecution.end === currentExecutionTime) {
+      lastExecution.end++;
     } else {
       process.executions.push({
         start: currentExecutionTime,
         end: currentExecutionTime + 1
       });
     }
+
     process.burstTime--;
   }
 
-  let totalWaitingTime = 0;
-  processes.forEach(process => {
-    for (
-      let executionIndex = 0;
-      executionIndex < process.executions.length;
-      executionIndex++
-    ) {
-      let execution = process.executions[executionIndex];
-      if (executionIndex === 0) {
-        totalWaitingTime += execution.start - process.arrivalTime;
-      } else {
-        let previousExecution = process.executions[executionIndex - 1];
-        totalWaitingTime += execution.start - previousExecution.end;
-      }
-    }
-  });
+  let totalWaitingTime = getTotalWaitingTimeProcesses(processes);
 
   return totalWaitingTime / processes.length;
 };
